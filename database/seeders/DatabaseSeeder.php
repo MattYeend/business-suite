f3e7f73764c417f152688b05b20a66c87c739023
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-04-30 09:01:33 +0100

    #9 Update to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        RolesAndPermissionsSeeder::class;
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+        ]);
         // User::factory(10)->create();
 
         // User::factory()->create([
