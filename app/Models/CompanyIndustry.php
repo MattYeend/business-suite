cb3a1b1a39b5cc14a883556e43b6fb5769d954a3
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-04 13:32:53 +0100

    #12 Update to company industry model

--- app/Models/CompanyIndustry.php
+++ app/Models/CompanyIndustry.php
@@ -2,11 +2,93 @@
 
 namespace App\Models;
 
+use Database\Factories\CompanyIndustryFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[Fillable([
+    'name',
+    'slug',
+    'created_by',
+    'updated_by',
+    'deleted_by',
+    'restored_by',
+    'restored_at',
+])]
+/**
+ * CompanyIndustry model for categorizing companies by industry.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ * @property int|null $deleted_by
+ * @property int|null $restored_by
+ * @property \Illuminate\Support\Carbon|null $restored_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ *
+ * @property-read User|null $creator
+ * @property-read User|null $updater
+ * @property-read User|null $deleter
+ * @property-read User|null $restorer
+ */
 class CompanyIndustry extends Model
 {
-    /** @use HasFactory<\Database\Factories\CompanyIndustryFactory> */
-    use HasFactory;
+    /**
+     * @use HasFactory<CompanyIndustryFactory>
+     * @use SoftDeletes<SoftDeletes>
+     */
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Get the user who created the industry.
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Get the user who last updated the industry.
+     */
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    /**
+     * Get the user who deleted the industry.
+     */
+    public function deleter()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+
+    /**
+     * Get the user who restored the industry.
+     */
+    public function restorer()
+    {
+        return $this->belongsTo(User::class, 'restored_by');
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string,string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'restored_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
 }
