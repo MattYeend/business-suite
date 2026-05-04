515a4b7bd203bb379d160a458f74c16d35278920
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-04 12:37:36 +0100

    #12 Company Industry blank files

---
+++ app/Http/Controllers/CompanyIndustryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCompanyIndustryRequest;
+use App\Http\Requests\UpdateCompanyIndustryRequest;
+use App\Models\CompanyIndustry;
+
+class CompanyIndustryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompanyIndustryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CompanyIndustry $companyIndustry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CompanyIndustry $companyIndustry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompanyIndustryRequest $request, CompanyIndustry $companyIndustry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CompanyIndustry $companyIndustry)
+    {
+        //
+    }
+}
---
+++ database/factories/CompanyIndustryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CompanyIndustry;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<CompanyIndustry>
+ */
+class CompanyIndustryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
