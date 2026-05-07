ccb87abb9fcd43c4b333b93b471e582ed4e33a9c
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-07 09:41:55 +0100

    #56 Update to companycontactpolicy

--- app/Policies/CompanyContactPolicy.php
+++ app/Policies/CompanyContactPolicy.php
@@ -4,16 +4,25 @@ namespace App\Policies;
 
 use App\Models\CompanyContact;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use App\Services\CompanyContacts\CompanyContactPolicyAuthorizationService;
 
 class CompanyContactPolicy
 {
+    protected CompanyContactPolicyAuthorizationService $authorizationService;
+
+    public function __construct(
+        CompanyContactPolicyAuthorizationService $authorizationService
+    ) {
+        $this->authorizationService = $authorizationService;
+    }
+
     /**
      * Determine whether the user can view any models.
+     * Only admins can view the list of company industries.
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
     }
 
     /**
@@ -21,7 +30,7 @@ class CompanyContactPolicy
      */
     public function view(User $user, CompanyContact $companyContact): bool
     {
-        return false;
+        return $this->authorizationService->canView($user, $companyContact);
     }
 
     /**
@@ -29,7 +38,7 @@ class CompanyContactPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
     }
 
     /**
@@ -37,7 +46,7 @@ class CompanyContactPolicy
      */
     public function update(User $user, CompanyContact $companyContact): bool
     {
-        return false;
+        return $this->authorizationService->canUpdate($user, $companyContact);
     }
 
     /**
@@ -45,7 +54,7 @@ class CompanyContactPolicy
      */
     public function delete(User $user, CompanyContact $companyContact): bool
     {
-        return false;
+        return $this->authorizationService->canDelete($user, $companyContact);
     }
 
     /**
@@ -53,14 +62,51 @@ class CompanyContactPolicy
      */
     public function restore(User $user, CompanyContact $companyContact): bool
     {
-        return false;
+        return $this->authorizationService->canRestore($user, $companyContact);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, CompanyContact $companyContact): bool
+    public function forceDelete(
+        User $user,
+        CompanyContact $companyContact
+    ): bool {
+        return $this->authorizationService->canForceDelete(
+            $user,
+            $companyContact
+        );
+    }
+
+    /**
+     * Determine whether the user can bulk delete models.
+     */
+    public function bulkDelete(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can bulk restore models.
+     */
+    public function bulkRestore(User $user): bool
+    {
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can import models.
+     */
+    public function import(User $user): bool
+    {
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can export models.
+     */
+    public function export(User $user): bool
+    {
+        return $this->authorizationService->isUser($user);
     }
 }
