af71cf6b8e5923931df1fde63464d2bc07af3885
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-09 11:32:58 +0100

    #58 Update to policy

--- app/Policies/CompanyAddressPolicy.php
+++ app/Policies/CompanyAddressPolicy.php
@@ -4,16 +4,25 @@ namespace App\Policies;
 
 use App\Models\CompanyAddress;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use App\Services\CompanyAddresses\CompanyAddressPolicyAuthorizationService;
 
 class CompanyAddressPolicy
 {
+    protected CompanyAddressPolicyAuthorizationService $authorizationService;
+
+    public function __construct(
+        CompanyAddressPolicyAuthorizationService $authorizationService
+    ) {
+        $this->authorizationService = $authorizationService;
+    }
+
     /**
      * Determine whether the user can view any models.
+     * Only admins can view the list of company industries.
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
     }
 
     /**
@@ -21,7 +30,7 @@ class CompanyAddressPolicy
      */
     public function view(User $user, CompanyAddress $companyAddress): bool
     {
-        return false;
+        return $this->authorizationService->canView($user, $companyAddress);
     }
 
     /**
@@ -29,7 +38,7 @@ class CompanyAddressPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
     }
 
     /**
@@ -37,7 +46,7 @@ class CompanyAddressPolicy
      */
     public function update(User $user, CompanyAddress $companyAddress): bool
     {
-        return false;
+        return $this->authorizationService->canUpdate($user, $companyAddress);
     }
 
     /**
@@ -45,7 +54,7 @@ class CompanyAddressPolicy
      */
     public function delete(User $user, CompanyAddress $companyAddress): bool
     {
-        return false;
+        return $this->authorizationService->canDelete($user, $companyAddress);
     }
 
     /**
@@ -53,14 +62,51 @@ class CompanyAddressPolicy
      */
     public function restore(User $user, CompanyAddress $companyAddress): bool
     {
-        return false;
+        return $this->authorizationService->canRestore($user, $companyAddress);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, CompanyAddress $companyAddress): bool
+    public function forceDelete(
+        User $user,
+        CompanyAddress $companyAddress
+    ): bool {
+        return $this->authorizationService->canForceDelete(
+            $user,
+            $companyAddress
+        );
+    }
+
+    /**
+     * Determine whether the user can bulk delete models.
+     */
+    public function bulkDelete(User $user): bool
     {
-        return false;
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can bulk restore models.
+     */
+    public function bulkRestore(User $user): bool
+    {
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can import models.
+     */
+    public function import(User $user): bool
+    {
+        return $this->authorizationService->isAdmin($user);
+    }
+
+    /**
+     * Determine whether the user can export models.
+     */
+    public function export(User $user): bool
+    {
+        return $this->authorizationService->isUser($user);
     }
 }
