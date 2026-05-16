fbe76c2c51d71f91bfa27920aff787a1409e018d
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-16 08:39:21 +0100

    #80 New indexes for company contacts

--- database/migrations/2026_05_16_073803_add_indexes_to_company_contacts_table.php
+++ database/migrations/2026_05_16_073803_add_indexes_to_company_contacts_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('company_contacts', function (Blueprint $table) {
-            //
+            $table->index(['first_name', 'last_name']);
+            $table->index('email');
+            $table->index('is_primary');
         });
     }
 
@@ -22,7 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('company_contacts', function (Blueprint $table) {
-            //
+            $table->dropIndex(['company_contacts_first_name_last_name_index']);
+            $table->dropIndex(['company_contacts_email_index']);
+            $table->dropIndex(['company_contacts_is_primary_index']);
         });
     }
 };
